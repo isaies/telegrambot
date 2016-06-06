8e4262a92c2efc3e852480c499320f03b5296a31
isaies <isaies@gmail.com>
2016-06-06 14:07:39 +0200

    Readme file, start action

--- README.md
+++ README.md
@@ -1,1 +1,119 @@
 #TELEGRAM BOT EXAMPLE WITH NODE.JS
+
+This is a telegram bot example using Node.js using these modules
+
+**node-telegram-bot-api** - ([@yagop](https://github.com/yagop))
+[https://github.com/yagop/node-telegram-bot-api](https://github.com/yagop/node-telegram-bot-api)
+
+**https**
+[https://nodejs.org/api/https.html](https://nodejs.org/api/https.html)
+
+**fs**
+[https://nodejs.org/api/fs.html](https://nodejs.org/api/fs.html)
+
+## Setting up your bot
+
+
+### 1. Create a bot using @botfather
+
+Add the `@botfather` to your telegram contact list.
+You can add it directly by clicking here: [@botfather](http://telegram.me/botfather)
+
+
+- Write `/newbot`
+
+Now you can choose a name for your bot
+
+- Write your bot name
+
+- Finally, choose a bot name for your bot in this format `_name_bot`
+
+You will receive your bot url and a token. Copy and save your token for the next step.
+
+### 2. Create a file with your token
+
+If you want to use this example, write your token in json format and export it.
+You can make it by writing this unix command 
+
+```shell
+$ echo "module.exports = { token : 'YOUR_BOT_TOKEN'}" } > con.js
+```
+
+or create a new `con.js` file with this:
+
+```
+module.exports = {
+	token : YOUR_TOKEN
+}
+```
+
+### 3. Install all dependences 
+
+By now, I'm using only a external dependence. You can add it in two ways:
+
+**Using the `package.json` in this repository:**
+```
+npm install
+```
+
+_or_
+
+**Install modules manually**
+
+- node-telegram-bot-api
+```
+npm install node-telegram-bot-api --save
+```
+
+
+### 4. Add and share your telegram bot
+
+You can share and interact with your bot by adding it to your telegram contact list, just like you did before with @telegrambot
+http://telegram.me/your-bot-name
+
+
+
+## Bot actions in this example
+
+### **Messages**
+
+### /start
+
+This is the initial required action to wake up your bot. It gives you the current actions
+
+### /say _something_
+
+Simulates the echo function, the bot will answer the same word you wrote.
+
+### hola
+
+A simple text message, the bot will answer to you with a photo ;)
+
+
+### *** Voice ***
+
+If you send to the bot a voice recording, it will download it and store it in your `./voice/` directory.
+It will answer to you the recording duration and the file size
+
+
+## TODO:
+
+- Search for local files in /photo action 
+- Make a geo response in /place action
+- Manage inline queries
+- Voice speech recognition
+- Make a response when receiving a photo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -7,64 +7,116 @@ var https = require('https');
 var bot = new TelegramBot(con.token, {polling: true});
 
 
+// Get the bot info
+bot.getMe()
+.then(function(data){
+	 console.log(data);
+ })
+.catch(function(err){
+	console.log("Error: ",err);
+});
+
+
+
+
+// Welcome message, list of available actions
+bot.onText(/\/start/, function(msg){
+	
+	var fromId = msg.from.id;
+	var resp = "Hola!\n"+
+		"Acciones disponibles: \n"+
+		"/say mensaje \n"+
+		"hola\n"+
+		"";
+	bot.sendMessage(fromId, resp);
+			
+});
+
+
+
+
+
+
 
 // Command /say
 bot.onText(/\/say (.+)/, function (msg, match) {
+	
 	var fromId = msg.from.id;
 	
 	// message without /say
 	var resp = match[1];
-		
-	console.log(match[1]);
+	
 	bot.sendMessage(fromId, resp);
 });
 
 
 
 
-// For every message
+// For every message (now only responds to "hola" message
 bot.on('message', function (msg) {
 	var chatId = msg.chat.id;
 	// photo can be: a file path, a stream or a Telegram file_id
 	var photo = 'images/hola.png';
 	//console.log(msg.text);
 	if(msg.text!="hola"){
+		console.log(msg.text);
 		//bot.sendMessage(msg.from.id,"Escribe hola" );
 	}else{	
 		bot.sendPhoto(chatId, photo, {caption: 'Caracola'});
 	}
 });
 
-// Send a photo
+// Receive a photo
+// TODO: search for local photo matches
 bot.onText(/\/foto (.+)/, function(msg, match){
-	var fromId = msg.from.id;
+	
+		var fromId = msg.from.id;
 	var photoName = match[1];
+	var photos = [];
+
+	
 	var resp = "No tenemos fotos de:"+photoName;
 	console.log(match);
 	bot.sendMessage(fromId, resp);
 
 });
 
-// Trying inline answers
-bot.onText(/aa/, function(msg){
-		var results = ["un", "dos", "tres"];
-		var inlineQueryId= "23";
-		bot.answerInlineQuery(inlineQueryId, results).then(function(response){
-				console.log("entra");
-				});
-		});
+// TODO: Get geo and photos from name
+bot.onText(/\/place (.+)/, function(msg, match){
 
-// Speech to text (only stores file)
+	var fromId = msg.from.id;
+	var place = match[1];
+	console.log(place, match);
+
+	
+
+
+});
+
+// TODO: managing inline messages
+bot.onText("inlineTest", function(msg){
+		
+	var results = ["un", "dos", "tres"];
+	var inlineQueryId= "23";
+
+	bot.answerInlineQuery(inlineQueryId, results).then(function(response){
+		console.log("test");
+	});
+});
+
+// Downloads the voice file
+// TODO: speech transcription
 bot.on('voice', function(msg){
 
 	var fromId = msg.from.id;
 
-	// General response
-	var resp="No te entiendo";
 	
 	// Voice file duration
 	var sec=msg.voice.duration;
 	var maxSec=40;
+	
+	// General response
+	var resp="Has estado hablando "+sec+" segundos.\n El tamaño total de tu grabación es de: "+msg.voice.file_size;
 	
 	// Avoid long files
 	if(msg.voice.file_size>100000){
